1692e6c3f57691744051c166b2f32b781d40e778
Tharun <tharunpkd@gmail.com>
2021-12-01 11:26:06 +0530

    commented

--- src/main/java/com/jxconvert/mainClass.java
+++ src/main/java/com/jxconvert/mainClass.java
@@ -3,26 +3,31 @@ package com.jxconvert;
 import com.jxconvert.utils.toJson;
 import com.jxconvert.utils.toXML;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class mainClass {
     public static void main(String[] args) throws IOException {
-        String jsonIn = "data/input/jsonSample.json";
-        String xmlIn = "data/input/xmlSample.xml";
-        String xmlOut = "data/output/Xml_OP.xml";
-        String jsonOut = "data/output/json_OP.json";
+        String jsonIn = "data/jsonSample.json";
+        String xmlIn = "data/xmlSample.xml";
 
         toJson obj1=new toJson();
         toXML obj2=new toXML();
 
-        FileWriter fp1 = new FileWriter(jsonOut);
-        fp1.write(obj1.convertToJson(xmlIn));
-        fp1.close();
+        File file = new File(jsonIn);
+        String parent = file.getParent();
 
-        FileWriter fp2 = new FileWriter(xmlOut);
-        fp2.write(obj2.convertToXML(jsonIn));
-        fp2.close();
+        System.out.println(parent.toString());
+
+//        FileWriter fp1 = new FileWriter(Paths.get(jsonIn).toAbsolutePath().toString()+ "/jsonOP.json");
+//        fp1.write(obj1.convertToJson(xmlIn));
+//        fp1.close();
+//
+//        FileWriter fp2 = new FileWriter(xmlOut);
+//        fp2.write(obj2.convertToXML(jsonIn));
+//        fp2.close();
     }
 
 }
