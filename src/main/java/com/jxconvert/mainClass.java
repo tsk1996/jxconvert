ac2ebf6412a57114f420711fa9f58ccd942aad80
Tharun <tharunpkd@gmail.com>
2021-12-01 18:51:33 +0530

    commented

--- src/main/java/com/jxconvert/mainClass.java
+++ src/main/java/com/jxconvert/mainClass.java
@@ -4,11 +4,8 @@ import com.jxconvert.utils.fileOps;
 import com.jxconvert.utils.toJson;
 import com.jxconvert.utils.toXML;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Locale;
+
 
 public class mainClass {
     public static void main(String[] args) throws IOException {
@@ -30,14 +27,14 @@ public class mainClass {
     }
 
     private static Integer optionSelect(String[] args) {
-        Integer option = 0;
+        int option = 0;
 
         if (args.length != 2) {
             System.err.println("Arguments count mismatch");
             System.err.println("Example: java -jar jarfilename.jar sourcetype filepath");
             System.exit(1);
         } else {
-            String arg = args[0].toString().toLowerCase();
+            String arg = args[0].toLowerCase();
             if (arg.contains("json")) {
                 option = 1;
             } else if (arg.contains("xml")) {
@@ -51,22 +48,22 @@ public class mainClass {
         return option;
     }
 
-    private static void oldMain() throws IOException {
-        String jsonIn = "data/jsonSample.json";
-        String xmlIn = "data/xmlSample.xml";
-        String xmlOut = "data/xmlOP.xml";
-        String jsonOut = "data/jsonOP.xml";
-
-        toJson obj1 = new toJson();
-        toXML obj2 = new toXML();
-
-        FileWriter fp1 = new FileWriter(jsonOut);
-        fp1.write(obj1.convertToJson(xmlIn));
-        fp1.close();
-
-        FileWriter fp2 = new FileWriter(xmlOut);
-        fp2.write(obj2.convertToXML(jsonIn));
-        fp2.close();
-    }
+//    private static void oldMain() throws IOException {
+//        String jsonIn = "data/jsonSample.json";
+//        String xmlIn = "data/xmlSample.xml";
+//        String xmlOut = "data/xmlOP.xml";
+//        String jsonOut = "data/jsonOP.xml";
+//
+//        toJson obj1 = new toJson();
+//        toXML obj2 = new toXML();
+//
+//        FileWriter fp1 = new FileWriter(jsonOut);
+//        fp1.write(obj1.convertToJson(xmlIn));
+//        fp1.close();
+//
+//        FileWriter fp2 = new FileWriter(xmlOut);
+//        fp2.write(obj2.convertToXML(jsonIn));
+//        fp2.close();
+//    }
 
 }
