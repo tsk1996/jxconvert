68a79fdfaac86188db1942bafd748b85837f0c27
Tharun <tharunpkd@gmail.com>
2022-05-03 14:21:27 +0530

    Updated to Junit5

--- src/test/java/convertTest.java
+++ src/test/java/convertTest.java
@@ -1,11 +1,15 @@
 import com.jxconvert.convert;
-import org.junit.Test;
-import org.xml.sax.SAXParseException;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.io.IOException;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class convertTest {
     @Test
+    @Order(1)
     public void xml2jsonTest() throws IOException {
         System.out.println("Test 1 : XML to Json Conversion");
         String xmlSource = "data/xmlSample.xml";
@@ -16,6 +20,7 @@ public class convertTest {
     }
 
     @Test
+    @Order(2)
     public void json2xmlTest() throws IOException {
         System.out.println("Test 2 : Json to XML Conversion");
         String jsonSource = "data/jsonSample.json";
