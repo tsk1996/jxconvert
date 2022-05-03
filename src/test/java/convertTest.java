0195b955b98650c770720e7d40cdb26dd4242c50
Tharun <tharunpkd@gmail.com>
2022-05-03 14:57:54 +0530

    Reverted to Junit4

--- src/test/java/convertTest.java
+++ src/test/java/convertTest.java
@@ -1,26 +1,26 @@
 import com.jxconvert.convert;
 import org.junit.Test;
-import org.xml.sax.SAXParseException;
 
 import java.io.IOException;
 
 public class convertTest {
-    @Test
-    public void xml2jsonTest() throws IOException {
-        System.out.println("Test 1 : XML to Json Conversion");
-        String xmlSource = "data/xmlSample.xml";
 
-        String[] args = {"xml", xmlSource};
+    @Test
+    public void json2xmlTest() throws IOException {
+        System.out.println("Test : Json to XML Conversion");
+        String jsonSource = "data/jsonSample.json";
+
+        String[] args = {"json", jsonSource};
 
         convert.main(args);
     }
 
     @Test
-    public void json2xmlTest() throws IOException {
-        System.out.println("Test 2 : Json to XML Conversion");
-        String jsonSource = "data/jsonSample.json";
+    public void xml2jsonTest() throws IOException {
+        System.out.println("Test : XML to Json Conversion");
+        String xmlSource = "data/xmlSample.xml";
 
-        String[] args = {"json", jsonSource};
+        String[] args = {"xml", xmlSource};
 
         convert.main(args);
     }
